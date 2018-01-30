8f3c1dd763cf66f5bb1e4844f3f9d3736e59d975
jesusdelrioc <jesusdelrioc@gmail.com>
2018-01-30 09:14:00 +0100

    iteraccion 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,45 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
+    this.interval; 
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+this.interval = setInterval(() => {
+  this.floorUp();
+  this.update();
+},1000);
+  }
+stop() { }
+  update() {
+     return this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+  floorUp() {
+    if (this.floor < this.MAXFLOOR && this.direction == "up"){
+      this.floor++;
+    }else{
+      this.direction = "down";
+      this.floorDown();
+    }
+  }
+  floorDown() {
+    if (this.floor > 0 && this.direction == "down"){
+      this.floor--;
+    }else{
+      this.direction = "up";
+      this.floorUp();
+    }
+  }
   call() { }
-  log() { }
+  log() {
+    console.log(`Direction : ${this.direction}`);
+    console.log(`Floor : ${this.floor}`); 
+   }
 }
 
 module.exports = Elevator;
